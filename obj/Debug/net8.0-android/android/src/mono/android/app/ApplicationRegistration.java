5127f21eff05a76be95280c0e2b56b4afd9406fa
Patrik Kurniawan Saputra <139532729+patriksaputraaa@users.noreply.github.com>
2024-09-26 14:45:46 +0700

    Adding HorizontalStackLayout

---
+++ obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("TipCalculator.MainApplication, TipCalculator, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64ac31dba612cdbb23.MainApplication.class, crc64ac31dba612cdbb23.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
